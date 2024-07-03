1e822ef0f47214b7184aa2e02a17685f09b71885
unknown <designkrunchstudio@gmail.com>
2024-07-03 18:12:04 +0530

    fixed null value product issue

--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -3,26 +3,32 @@ import Products from "@/app/features/products";
 import { client } from "@/app/utils/configSanity";
 
 const getData = async (data1: any) => {
-  console.log({ asdasda: data1 });
-
+  console.log("Received search parameters:", data1);
   const query = `*[_type == "hanger" &&
-  (!defined("${data1?.material}") || material->name == "${data1?.material}") ||
-  (!defined("${data1?.style}") || styletype[0]->name == "${data1?.style}") ||
-  (!defined("${data1?.specialty}") || specialty[0]->name == "${data1?.specialty}") ||
-  (!defined("${data1.hook}") || hook[0]->name == "${data1.hook}")
+  (!defined($materialType) || material->name == $materialType) &&
+  (!defined($styleType) || styletype[0]->name == $styleType) &&
+  (!defined($specialtyHangerType) || specialty[0]->name == $specialtyHangerType) &&
+  (!defined($hookType) || hook[0]->name == $hookType)
 ] | order(_createdAt) {
-   name, length, height, 
+  name, length, height,
   "images": images[].asset->url,
   "styletype": styletype[]->name,
   "material": material->name,
   "hook": hook[]->name,
   "specialty": specialty[]->name,
-}
-`;
+}`;
 
-  const data = await client.fetch(query);
+  const params = {
+    materialType: data1?.material || null,
+    styleType: data1?.style || null,
+    specialtyHangerType: data1?.specialty || null,
+    hookType: data1?.hook || null,
+  };
+
+  const data = await client.fetch(query, params);
   return data;
 };
+
 const getFilters = async () => {
   const styleTypes = `*[_type == "styleType"] | order(_createdAt){ name }`;
   const specialtyType = `*[_type == "specialtyHangerType"] | order(_createdAt){ name }`;
@@ -40,6 +46,7 @@ const getFilters = async () => {
     hookTypes: res4.map((e: any) => e?.name),
   };
 };
+
 const Page = async (request: any) => {
   const data = await getData(request?.searchParams);
   const filters = await getFilters();
