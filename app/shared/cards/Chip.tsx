e4cec5dbc21da9203efa07ec75dce842a29dba7a
S-aeriax <os001@aerial.world>
2024-07-08 00:25:59 +0530

    FEAT: added filters

--- app/shared/cards/Chip.tsx
+++ app/shared/cards/Chip.tsx
@@ -13,7 +13,7 @@ const Chip = ({
   return (
     <div
       className={`bg-primary2 cursor-pointer gap-x-2 flex items-center text-sm pl-4 pr-3 py-1 text-white rounded-full ${className}`}
-      onClick={className && onClick}
+      onClick={className ? () => onClick : () => {}}
     >
       <div>{title}</div>
       {!className && <RxCross2 size={16} onClick={onClick} />}
